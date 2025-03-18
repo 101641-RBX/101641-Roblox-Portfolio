ea30a07c8dc823b7b3b41e8cb8181301a207b0f3
101641 <scp00347@gmail.com>
2025-03-18 15:59:08 -0700

    Update and rename README.html to README.m

---
+++ README.m
@@ -0,0 +1,25 @@
+# 101641 Roblox Portfolio
+
+Developing on Roblox since December 2022. I have been on Roblox since 2009 and have a big history with this game. Folders attached to this repository will go over the different areas I have done scripting, including examples.
+Currently pursing software engineering as a degree.
+
+# Experience Rundown
+Below you can find a quick rundown of what I have experience in coding:
+- Programming tools, weapons, objects
+- Programming NPC pathfinding and behavior (Highest Focus)
+- Programming interactive GUI's
+- Organizing and Optimizing code via module scripts
+
+I also have done a light amount of work in the following fields, mostly for placeholder purposes for my code:
+- Create interactive GUI's
+- Animate R15 & R6 rigs
+
+
+# Handles For Contact
+Here are social links if you need to get in contact with me:
+- Roblox Profile: https://www.roblox.com/users/5621075/profile
+- Discord: 101641
+
+# Notice
+
+Keep in mind I do have a IRL job and college, so while I can still complete stuff in a resonable time, delays can occur.
